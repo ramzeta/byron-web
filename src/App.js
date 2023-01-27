ed11954d32c9d4c730af7fd7f38b19af92fe7689
ramzeta <ramiroperezsanz13@gmail.com>
2023-01-27 20:16:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import background from "./images/car.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Nav
+          activeKey="/home"
+          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
         >
-          Learn React
-        </a>
+          <Nav.Item>
+            <Nav.Link href="/home">Inicio</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Tecnologias</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Contacto</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+          <Nav.Link eventKey="link-2">Sobre Nosotros</Nav.Link>
+          </Nav.Item>
+        </Nav>
       </header>
+      <body>
+        <div style={{
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: 'no-repeat',
+          width:'auto'
+        }}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      </body>
+      <footer>
+
+      </footer>
     </div>
   );
 }
